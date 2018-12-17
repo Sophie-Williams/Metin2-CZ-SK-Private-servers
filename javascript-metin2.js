d40cdc9b3248bbb04737c9a29846102140de4311
filipopl <filipopl@seznam.cz>
2018-12-17 23:34:58 +0100

    Adding audio, few images and some new servers

--- javascript-metin2.js
+++ javascript-metin2.js
@@ -138,3 +138,13 @@ function AccountSort() {
   }
 }
 
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
